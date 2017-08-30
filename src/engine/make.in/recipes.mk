45f83a0f97dfdd922148ef8e520ccace945e41e7
Joe Colosimo <jwc@jwcxz.com>
2017-08-30 10:47:57 -0700

    page assembly recipe: escape wildcard in secondary expansion, exclude infra prereqs

--- src/engine/make.in/recipes.mk
+++ src/engine/make.in/recipes.mk
@@ -8,7 +8,9 @@ $(OUT_STATIC_CONTENT): $(DIR_OUT)/%: $(DIR_CONTENT)/% $(MK_INFRA_PREREQS)
 	$(LC)cp $< $@ $(LL)
 
 .SECONDEXPANSION:
-$(DIR_OUT)/%/$(PAGE_FILE): $(DIR_CONTENT)/%/$(PAGE_METADATA) $$(filter $$(patsubst $(DIR_OUT)%,$(DIR_BUILD_FRAG)%,$$(dir $$%))%,$(BUILD_FRAG_FILES)) $(MK_INFRA_PREREQS)
+# escape wildcard for use in second expansion (https://stackoverflow.com/questions/25589586)
+WC := %
+$(DIR_OUT)/%/$(PAGE_FILE): $(DIR_CONTENT)/%/$(PAGE_METADATA) $$(filter $$(patsubst $(DIR_OUT)$$(WC),$(DIR_BUILD_FRAG)$$(WC),$$(DIR_ROOT)/$$(dir $$@))$$(WC),$(BUILD_FRAG_FILES)) $(MK_INFRA_PREREQS)
 	$(call msg_gen,$@)
 	$(LC)mkdir -p $(dir $@) $(LL)
-	$(LC)$(T_ASSEMBLE) -o $@ $(addprefix -d ,$(dir $(filter-out $<, $^))) $< $(LL)
+	$(LC)$(T_ASSEMBLE) -o $@ $(addprefix -d ,$(dir $(filter-out $(MK_INFRA_PREREQS),$(addprefix $(DIR_ROOT)/,$(filter-out $<, $^))))) $< $(LL)
