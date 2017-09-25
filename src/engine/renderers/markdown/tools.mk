232f4baf8c58c00a05b8ad3120bd95734da03b6f
Joe Colosimo <jwc@jwcxz.com>
2017-09-25 01:33:10 -0700

    add support for ToC to markdown rendering

--- src/engine/renderers/markdown/tools.mk
+++ src/engine/renderers/markdown/tools.mk
@@ -1,4 +1,5 @@
-T_RENDER_MARKDOWN := $(DIR_ROOT)/node_modules/.bin/markdown-it
+T_RENDER_MARKDOWN_SCRIPT := $(DIR_RENDERERS)/markdown/markdown-it
+T_RENDER_MARKDOWN := node $(T_RENDER_MARKDOWN_SCRIPT)
 
 # TODO: technically, this should be all portions of the tool, including dependencies
-$(call mk_add_infra_prereq,$(T_RENDER_MARKDOWN))
+$(call mk_add_infra_prereq,$(T_RENDER_MARKDOWN_SCRIPT))
