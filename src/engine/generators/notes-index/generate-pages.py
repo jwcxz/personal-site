a9ad9ed1a112fb51b8023fb1b59e7940d51bd828
Joe Colosimo <jwc@jwcxz.com>
2017-10-04 22:35:32 -0700

    move renderutils to a uitools subpath

--- src/engine/generators/notes-index/generate-pages.py
+++ src/engine/generators/notes-index/generate-pages.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 
 from notes.notes import Note, NotesList
-from renderutils import encode_for_json, make_page_navbar
+from uitools.renderutils import encode_for_json, make_page_navbar
 
 
 page_template = """
