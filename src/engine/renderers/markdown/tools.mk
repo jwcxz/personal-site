694d2bbce61ec54cb74a3ef2745d7773b0305105
Joe Colosimo <jwc@jwcxz.com>
2017-10-10 21:49:08 -0700

    enable linkify-it and typographer when rendering markdown

--- src/engine/renderers/markdown/tools.mk
+++ src/engine/renderers/markdown/tools.mk
@@ -1,5 +1,5 @@
 T_RENDER_MARKDOWN_SCRIPT := $(DIR_RENDERERS)/markdown/markdown-it
-T_RENDER_MARKDOWN := node $(T_RENDER_MARKDOWN_SCRIPT)
+T_RENDER_MARKDOWN := node $(T_RENDER_MARKDOWN_SCRIPT) -l -t
 
 # TODO: technically, this should be all portions of the tool, including dependencies
 $(call mk_add_infra_prereq,$(T_RENDER_MARKDOWN_SCRIPT))
