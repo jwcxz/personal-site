c9ae4f7ca627e8cf46c7bf669020766dfff241e0
Joe Colosimo <jwc@jwcxz.com>
2017-11-17 21:15:17 -0800

    add thumbnails for png images

--- src/engine/derivers/img-thumbs/files.mk
+++ src/engine/derivers/img-thumbs/files.mk
@@ -1,3 +1,6 @@
-ALL_IMAGES := $(shell find $(DIR_CONTENT) -iname "*.jpg")
+ALL_IMAGES_JPG := $(shell find $(DIR_CONTENT) -iname "*.jpg")
+ALL_IMAGES_PNG := $(shell find $(DIR_CONTENT) -iname "*.png")
 
-OUT_DERIVED_THUMBS := $(ALL_IMAGES:$(DIR_CONTENT)/%=$(DIR_OUT)/%.thumb.jpg)
+ALL_IMAGES := $(ALL_IMAGES_JPG) $(ALL_IMAGES_PNG)
+
+OUT_DERIVED_THUMBS := $(ALL_IMAGES_JPG:$(DIR_CONTENT)/%=$(DIR_OUT)/%.thumb.jpg) $(ALL_IMAGES_PNG:$(DIR_CONTENT)/%=$(DIR_OUT)/%.thumb.png)
--- src/engine/derivers/img-thumbs/recipes.mk
+++ src/engine/derivers/img-thumbs/recipes.mk
@@ -3,4 +3,9 @@ $(DIR_OUT)/%.thumb.jpg: $(DIR_CONTENT)/% $(MK_INFRA_PREREQS)
 	$(LC)mkdir -p $(dir $@) $(LL)
 	$(LC)$(T_DERIVE_RESIZED_IMG) $< -resize $(IMG_MAX_WIDTH)x\> $@ $(LL)
 
+$(DIR_OUT)/%.thumb.png: $(DIR_CONTENT)/% $(MK_INFRA_PREREQS)
+	$(call msg_gen,$@)
+	$(LC)mkdir -p $(dir $@) $(LL)
+	$(LC)$(T_DERIVE_RESIZED_IMG) $< -resize $(IMG_MAX_WIDTH)x\> $@ $(LL)
+
 all-targets: $(OUT_DERIVED_THUMBS)
